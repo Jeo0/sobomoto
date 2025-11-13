ab347ab0fd2bee8d01d283ef7a77fc494538508a
Jeo0 <jjrelb@gmail.com>
2025-11-13 14:02:49 +0800

    added kenenth esp32cam mac

--- receiver-final/recevier-final/recevier-final.ino
+++ receiver-final/recevier-final/recevier-final.ino
@@ -19,15 +19,29 @@
 // for ESP 32 CAM
 // EN_A and EN_B are pulled up by default (HIGH)
 // #define EN_A        19
-#define MOTOR_1    2  // 1 R_FORWARD
-#define MOTOR_2    14  // 2 R_BACKWARD
-#define MOTOR_3    15  // 3 L_FORWARD
-#define MOTOR_4    13  // 4 L_BACKWARD
+// #define MOTOR_1    2  // 1 R_FORWARD
+// #define MOTOR_2    14  // 2 R_BACKWARD
+// #define MOTOR_3    15  // 3 L_FORWARD
+// #define MOTOR_4    13  // 4 L_BACKWARD
 // #define EN_B        4
 
 // spin motor (attack)
-#define IN_A    12
-#define IN_B    4
+// #define IN_A    12
+// #define IN_B    4
+
+// =========================================================
+// for ESP 32 CAM NI LKENENTH
+// EN_A and EN_B are pulled up by default (HIGH)
+// #define EN_A        2
+#define MOTOR_1    33 // 1 R_FORWARD
+#define MOTOR_2    15  // 2 R_BACKWARD
+#define MOTOR_3    14  // 3 L_FORWARD
+#define MOTOR_4    13  // 4 L_BACKWARD
+// #define EN_B        12
+
+// spin motor (attack)
+#define IN_A    2
+#define IN_B    12
 
 
 
